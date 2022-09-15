010529f3d37826091b2623b41ec8aa81133d63d1
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-09-15 18:29:29 +0530

    c

--- lib/Screens/DialPad_Screen.dart
+++ lib/Screens/DialPad_Screen.dart
@@ -1,8 +1,4 @@
-// ignore_for_file: file_names, camel_case_types
-// ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
-
 
 class Dail_Screen extends StatefulWidget {
   const Dail_Screen({super.key});
@@ -14,6 +10,130 @@ class Dail_Screen extends StatefulWidget {
 class _Dail_ScreenState extends State<Dail_Screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(231, 29, 42, 85),
+        centerTitle: true,
+        title: Text(
+          'DIALER',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      body: Container(
+        width: 700,
+        height: 900,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff1E294E),
+              Colors.white,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFields('Name'),
+              TextFields('Email'),
+              TextFields('Number'),
+              //Row 1
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Dial_Pad(Colors.yellow, Colors.pink, 1),
+                  Dial_Pad(Colors.green, Colors.blue, 2),
+                  Dial_Pad(Colors.red, Colors.blue, 3),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Dial_Pad(Colors.yellow, Colors.pink, 4),
+                  Dial_Pad(Colors.green, Colors.blue, 5),
+                  Dial_Pad(Colors.red, Colors.blue, 6),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Dial_Pad(Colors.yellow, Colors.pink, 7),
+                  Dial_Pad(Colors.green, Colors.blue, 8),
+                  Dial_Pad(Colors.red, Colors.blue, 9),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Dial_Pad(Colors.green, Colors.greenAccent, 'Call'),
+                  Dial_Pad(Colors.green, Colors.blue, 0),
+                  Dial_Pad(Colors.red, Colors.redAccent, 'Remove'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget TextFields(String text) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 8,
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: text,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget Dial_Pad(Colour, Colours, numbers) {
+    return Container(
+      width: 120,
+      height: 130,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colour, Colours],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          numbers.toString(),
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: file_names, camel_case_types
 // ignore_for_file: file_names
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -13,9 +14,102 @@ class Home_Screen extends StatefulWidget {
 class _Home_ScreenState extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Scaffold(
+      backgroundColor: Color(0xff1E294E),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 200,
+        title: Text(
+          'UpOnly\nWelcomes you!',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          width: 700,
+          height: 900,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            color: Colors.white,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                cards('Call Module', 'assets/call.png',
+                    Color.fromARGB(255, 0, 16, 1), Colors.green),
+                cards(
+                  'F2F',
+                  'assets/F2F.png',
+                  Color.fromARGB(255, 18, 52, 57),
+                  Colors.cyan,
+                ),
+                cards(
+                  'CRM',
+                  'assets/CRM.png',
+                  Color.fromARGB(223, 47, 42, 2),
+                  Color.fromARGB(208, 247, 224, 10),
+                ),
+                cards(
+                  'HCMS',
+                  'assets/HCMS.png',
+                  Color.fromARGB(223, 47, 42, 2),
+                  Color.fromARGB(208, 247, 224, 10),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget cards(cardTitle, cardImage, g1Color, g2Color) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Container(
-        child: Column(),
+        width: 720,
+        height: 80,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [g1Color, g2Color],
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                cardTitle,
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Image.asset(
+              cardImage,
+              height: 50,
+              width: 80,
+            ),
+          ],
+        ),
       ),
     );
   }
